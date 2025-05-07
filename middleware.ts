1b53a7c3900fc00b2cb99c8fb65851e35119e25b
ilkayGkbdk <ilkayrg55@hotmail.com>
2025-05-07 12:52:15 +0300

    clerk updates

--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,24 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
 const isPublicRoute = createRouteMatcher([
     '/',
     '/sign-in(.*)',
     '/sign-up(.*)',
     '/about',
-    '/contact'
-])
+    '/contact',
+]);
 
 export default clerkMiddleware(async (auth, request) => {
     if (!isPublicRoute(request)) {
-        await auth.protect()
+        await auth.protect({
+            unauthenticatedUrl: '/sign-in', // Giriş yapmamış kullanıcılar buraya yönlendirilir
+        });
     }
-})
+});
 
 export const config = {
     matcher: [
         '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
         '/(api|trpc)(.*)',
     ],
-}
+};
